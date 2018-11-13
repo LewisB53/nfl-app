6c16d4883c0017487d63e70115f8b453f5153a67
Lewis Burns <lewis_b53b@hotmail.co.uk>
2018-11-13 20:43:38 +0000

    console log one player

--- src/App.js
+++ src/App.js
@@ -20,16 +20,31 @@ class App extends Component {
 
   componentDidMount() {
     axios
-      .get("https://jsonplaceholder.typicode.com/users")
+      .get("http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2018&week=1&format=json")
       .then(response => {
 
         // create an array of contacts only with relevant data
-        const newContacts = response.data.map(c => {
-          return {
-            id: c.id,
-            name: c.name
-          };
-        });
+        console.log(response.data);
+
+        const wholeobject = response.data;
+        const findOnePlayer = wholeobject.players[213];
+        console.log(findOnePlayer.name);
+        console.log(findOnePlayer.weekPts);
+        console.log(findOnePlayer.position);
+        console.log(findOnePlayer.stats);
+
+
+
+        // const playersArray = wholeobject.players;
+
+        // const newContacts = playersArray
+
+        // const newContacts = response.data.players.map(c => {
+        //   return {
+        //     id: c.id,
+        //     name: c.name
+        //   };
+        // });
 
         // create a new "State" object without mutating 
         // the original State object. 
