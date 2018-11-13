d4cbedc85cc9d320e47ee38e03e6584886fcfc88
Lewis Burns <lewis_b53b@hotmail.co.uk>
2018-11-13 15:02:18 +0000

    apiAccessDefault

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import "./App.css";
+import axios from "axios";
+
+import ContactList from "./components/ContactList";
+
+const contacts = [
+  { id: 1, name: "Leanne Graham" },
+  { id: 2, name: "Ervin Howell" },
+  { id: 3, name: "Clementine Bauch" },
+  { id: 4, name: "Patricia Lebsack" }
+];
+
+class App extends Component {
+
+  // default State object
+  state = {
+    contacts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+
+        // create an array of contacts only with relevant data
+        const newContacts = response.data.map(c => {
+          return {
+            id: c.id,
+            name: c.name
+          };
+        });
+
+        // create a new "State" object without mutating 
+        // the original State object. 
+        const newState = Object.assign({}, this.state, {
+          contacts: newContacts
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+        ...
+
+        <ContactList contacts={this.state.contacts} />
+      </div>
+    );
+  }
+}
+
+export default App;
